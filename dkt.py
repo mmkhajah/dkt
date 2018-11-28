5706ecbe85d597fd2d312f483d747c2d7345ca3d
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2018-11-28 16:19:45 -0700

    Removed Theano dependency, added python3 support, swapped depracted keras functions

--- dkt.py
+++ dkt.py
@@ -11,15 +11,14 @@
 #       dataset.txt.history training history (training LL, test AUC)
 #       dataset.txt.preds predictions for test trials
 #
-from keras.layers.core import TimeDistributedDense, Masking
-from keras.models import Sequential, Graph
+from keras.layers import TimeDistributed, Dense, Masking, LSTM
+from keras.losses import binary_crossentropy
 from keras.preprocessing import sequence
-from keras.layers.recurrent import LSTM
+from keras.models import Sequential
 from keras.utils import np_utils
 from keras import backend as K
 
 from sklearn.metrics import roc_auc_score
-import theano.tensor as Th
 import numpy as np
 
 import argparse
@@ -31,12 +30,12 @@ import os
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--dataset', type=str, help='Dataset file', required=True)
-    parser.add_argument('--splitfile', type=str, help='Split file', required=True)
-    parser.add_argument('--hiddenunits', type=int, help='Number of LSTM hidden units.', default=200, required=False)
-    parser.add_argument('--batchsize', type=int, help='Number of sequences to process in a batch.', default=5, required=False)
-    parser.add_argument('--timewindow', type=int, help='Number of timesteps to process in a batch.', default=100, required=False)
-    parser.add_argument('--epochs', type=int, help='Number of epochs.', default=50, required=False)
+    parser.add_argument('--dataset', '-d', type=str, help='Dataset file', required=True)
+    parser.add_argument('--splitfile', '-s', type=str, help='Split file', required=True)
+    parser.add_argument('--hiddenunits', '-u', type=int, help='Number of LSTM hidden units.', default=200, required=False)
+    parser.add_argument('--batchsize', '-b', type=int, help='Number of sequences to process in a batch.', default=5, required=False)
+    parser.add_argument('--timewindow', '-t', type=int, help='Number of timesteps to process in a batch.', default=100, required=False)
+    parser.add_argument('--epochs', '-e', type=int, help='Number of epochs.', default=50, required=False)
     args = parser.parse_args()
 
     dataset = args.dataset
@@ -56,12 +55,12 @@ def main():
 
     # load dataset
     training_seqs, testing_seqs, num_skills = load_dataset(dataset, split_file)
-    print "Training Sequences: %d" % len(training_seqs)
-    print "Testing Sequences: %d" % len(testing_seqs)
-    print "Number of skills: %d" % num_skills
+    print("Training Sequences: %d" % len(training_seqs))
+    print("Testing Sequences: %d" % len(testing_seqs))
+    print("Number of skills: %d" % num_skills)
 
     # Our loss function
-    # The model gives predictions for all skills so we need to get the 
+    # The model gives predictions for all skills so we need to get the
     # prediction for the skill at time t. We do that by taking the column-wise
     # dot product between the predictions at each time slice and a
     # one-hot encoding of the skill at time t.
@@ -70,7 +69,7 @@ def main():
     def loss_function(y_true, y_pred):
         skill = y_true[:,:,0:num_skills]
         obs = y_true[:,:,num_skills]
-        rel_pred = Th.sum(y_pred * skill, axis=2)
+        rel_pred = K.sum(y_pred * skill, axis=2)
 
         # keras implementation does a mean on the last dimension (axis=-1) which
         # it assumes is a singleton dimension. But in our context that would
@@ -82,29 +81,28 @@ def main():
     model = Sequential()
 
     # ignore padding
-    model.add(Masking(
-        -1.0, batch_input_shape=(batch_size, time_window, num_skills*2)))
+    model.add(Masking(-1.0, batch_input_shape=(batch_size, time_window, num_skills*2)))
 
     # lstm configured to keep states between batches
-    model.add(LSTM(input_dim=num_skills*2, 
-                   output_dim=hidden_units, 
-                   return_sequences=True,
-                   batch_input_shape=(batch_size, time_window, num_skills*2),
-                   stateful=True))
+    model.add(LSTM(
+        units=hidden_units,
+        input_shape=num_skills*2,
+        return_sequences=True,
+        batch_input_shape=(batch_size, time_window, num_skills*2),
+        stateful=True))
 
     # readout layer. TimeDistributedDense uses the same weights for all
     # time steps.
-    model.add(TimeDistributedDense(input_dim=hidden_units, 
-        output_dim=num_skills, activation='sigmoid'))
+    model.add(TimeDistributed(Dense(units=num_skills, activation='sigmoid',
+        input_shape=(hidden_units,))))
 
     # optimize with rmsprop which dynamically adapts the learning
     # rate of each weight.
-    model.compile(loss=loss_function,
-                optimizer='rmsprop',class_mode="binary")
+    model.compile(loss=loss_function, optimizer='rmsprop')
 
     # training function
     def trainer(X, Y):
-        overall_loss[0] += model.train_on_batch(X, Y)[0]
+        overall_loss[0] += model.train_on_batch(X, Y)
 
     # prediction
     def predictor(X, Y):
@@ -115,8 +113,8 @@ def main():
 
         rel_pred = np.sum(y_pred * skill, axis=2)
 
-        for b in xrange(0, X.shape[0]):
-            for t in xrange(0, X.shape[1]):
+        for b in range(0, X.shape[0]):
+            for t in range(0, X.shape[1]):
                 if X[b, t, 0] == -1.0:
                     continue
                 preds.append((rel_pred[b][t], obs[b][t]))
@@ -128,11 +126,11 @@ def main():
 
     # similiar to the above
     def finished_batch(percent_done):
-        print "(%4.3f %%) %f" % (percent_done, overall_loss[0])
+        print("(%4.3f %%) %f" % (percent_done, overall_loss[0]))
         model.reset_states()
 
     # run the model
-    for e in xrange(0, epochs):
+    for e in range(0, epochs):
         # train
         model.reset_states()
         run_func(training_seqs, num_skills, trainer, batch_size, time_window, finished_batch)
@@ -149,7 +147,7 @@ def main():
 
         # save model
         model.save_weights(model_file, overwrite=True)
-        print "==== Epoch: %d, Test AUC: %f" % (e, auc)
+        print("==== Epoch: %d, Test AUC: %f" % (e, auc))
 
         # reset loss
         overall_loss[0] = 0.0
@@ -177,15 +175,15 @@ def run_func(seqs, num_skills, f, batch_size, time_window, batch_done = None):
     random.shuffle(seqs)
 
     processed = 0
-    for start_from in xrange(0, len(seqs), batch_size):
+    for start_from in range(0, len(seqs), batch_size):
        end_before = min(len(seqs), start_from + batch_size)
        x = []
        y = []
        for seq in seqs[start_from:end_before]:
            x_seq = []
            y_seq = []
-           xt_zeros = [0 for i in xrange(0, num_skills*2)]
-           ct_zeros = [0 for i in xrange(0, num_skills+1)]
+           xt_zeros = [0 for i in range(0, num_skills*2)]
+           ct_zeros = [0 for i in range(0, num_skills+1)]
            xt = xt_zeros[:]
            for skill, is_correct in seq:
                x_seq.append(xt)
@@ -207,19 +205,19 @@ def run_func(seqs, num_skills, f, batch_size, time_window, batch_done = None):
        maxlen = round_to_multiple(maxlen, time_window)
        # fill up the batch if necessary
        if len(x) < batch_size:
-            for e in xrange(0, batch_size - len(x)):
+            for e in range(0, batch_size - len(x)):
                 x_seq = []
                 y_seq = []
-                for t in xrange(0, time_window):
-                    x_seq.append([-1.0 for i in xrange(0, num_skills*2)])
-                    y_seq.append([0.0 for i in xrange(0, num_skills+1)])
+                for t in range(0, time_window):
+                    x_seq.append([-1.0 for i in range(0, num_skills*2)])
+                    y_seq.append([0.0 for i in range(0, num_skills+1)])
                 x.append(x_seq)
                 y.append(y_seq)
 
        X = pad_sequences(x, padding='post', maxlen=maxlen, dim=num_skills*2, value=-1.0)
        Y = pad_sequences(y, padding='post', maxlen=maxlen, dim=num_skills+1, value=-1.0)
 
-       for t in xrange(0, maxlen, time_window):
+       for t in range(0, maxlen, time_window):
            f(X[:,t:(t+time_window),:], Y[:,t:(t+time_window),:])
 
        processed += end_before - start_from
@@ -239,8 +237,8 @@ def load_dataset(dataset, split_file):
     with open(split_file, 'r') as f:
         student_assignment = f.read().split(' ')
 
-    training_seqs = [seqs[i] for i in xrange(0, len(seqs)) if student_assignment[i] == '1']
-    testing_seqs = [seqs[i] for i in xrange(0, len(seqs)) if student_assignment[i] == '0']
+    training_seqs = [seqs[i] for i in range(0, len(seqs)) if student_assignment[i] == '1']
+    testing_seqs = [seqs[i] for i in range(0, len(seqs)) if student_assignment[i] == '0']
 
     return training_seqs, testing_seqs, num_skills
 
@@ -265,7 +263,7 @@ def read_file(dataset_path):
 
 
 # https://groups.google.com/forum/#!msg/keras-users/7sw0kvhDqCw/QmDMX952tq8J
-def pad_sequences(sequences, maxlen=None, dim=1, dtype='int32', padding='pre',truncating='pre', value=0.):
+def pad_sequences(sequences, maxlen=None, dim=1, dtype='int32', padding='pre', truncating='pre', value=0.):
     '''
         Override keras method to allow multiple feature dimensions.
         @dim: input feature dimension (number of features per timestep)
